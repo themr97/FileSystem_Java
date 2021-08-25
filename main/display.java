b64be65f9148f7a4fc796ccbbc1083c80f2a8ad7
Mahesh Ranpise <mahesh8276@gmail.com>
2021-08-25 14:16:51 +0530

    Add files via upload

--- main/display.java
+++ main/display.java
@@ -1,21 +1,26 @@
 package main;
 
 import java.io.*;
+import java.util.*;
 
 public class display {
 	
 	static void displayDir() {
-		File dir = new File("/home/maheshranpisein/Desktop/LockStorage/");
-		String[] files = dir.list();
-		if(files.length == 0) {
-			System.out.println("============> Looks like the root directory is empty!");
-		}else {
+		File fileDir = new File("/home/maheshranpisein/Desktop/LockStorage/");
+		if(fileDir.isDirectory()) {
+			List<String> listFile = Arrays.asList(fileDir.list());
 			System.out.println("========|FILES/FOLDER IN DIRECTORY|===============");
-			for(int i=0; i<files.length; i++) {
-				String filename = files[i];
-				System.out.println("| "+filename);
+			for(String s:listFile) {
+				System.out.println(s);
 			}
-			System.out.println("===================================================");
+			Collections.sort(listFile);
+			System.out.println("=========|	In Ascending Order	|============");
+			for(String s:listFile) {
+				System.out.println(s);
+			}
+		}
+		else {
+			System.out.println(fileDir.getAbsolutePath() + "is not present");
 		}
 	}
 
